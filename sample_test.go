9cf98bb9bf11a6baf20a4072087df6147d0153d1
hirofumi omi <omihirofumi@gmail.com>
2023-06-24 14:13:53 +0900

    fix test file

--- sample_test.go
+++ sample_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHelloWorld(t *testing.T) {
 	actual := HelloWorld("hoge")
-	expected := "hello world, hoge"
+	expected := "Hello world, hoge"
 	if actual != expected {
 		t.Errorf("actual %v, want %v", actual, expected)
 	}
